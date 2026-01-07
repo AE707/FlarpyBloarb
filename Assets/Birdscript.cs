4e08d50f6df559f9a62cfb744c1dfc26674eed3b
El.ghoul Alaa <ae7_07@outlook.com>
2026-01-08 00:33:15 +0300

    Initial commit: Unity project (clean)

---
+++ Assets/Birdscript.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Birdscript : MonoBehaviour
+{
+    public Rigidbody2D myRegidbody;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) ==true)
+        {
+            myRegidbody.linearVelocity = Vector2.up * 10;
+        }
+       
+    }
+}
