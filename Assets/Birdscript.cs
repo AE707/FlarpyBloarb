a44dff7eeca958e85595a5120af778cada12e0a9
El.ghoul Alaa <ae7_07@outlook.com>
2026-01-09 21:30:48 +0300

    Game Over Logic Implementation

--- Assets/Birdscript.cs
+++ Assets/Birdscript.cs
@@ -4,21 +4,30 @@ public class Birdscript : MonoBehaviour
 {
     public Rigidbody2D myRegidbody;
      public float flapStrength;
+     public LogicScript logic;
+     public bool birdIsAlive = true;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) ==true)
+        if (Input.GetKeyDown(KeyCode.Space) ==true && birdIsAlive == true)
         {
-            myRegidbody.linearVelocity = Vector2.up * 10;
+            myRegidbody.linearVelocity = Vector2.up * flapStrength;
         }
        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.GameOver();
+        birdIsAlive = false;
     }
 }
