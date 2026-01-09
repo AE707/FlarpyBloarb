44f61dca8ee337adfdd7a6fac0343a907dd89696
El.ghoul Alaa <ae7_07@outlook.com>
2026-01-09 21:35:05 +0300

    Game Logic Script Implementation

---
+++ Assets/LogicScript.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LogicScript : MonoBehaviour
+{
+   public int PlayerScore;
+   public Text scoreText;
+   public GameObject GameOverScreen;
+
+    [ContextMenu("Increase Score ")]
+   public void addScore(int scoreToAdd)
+    {
+        PlayerScore = PlayerScore + scoreToAdd;
+        scoreText.text = PlayerScore.ToString(); 
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void GameOver()
+    {
+        GameOverScreen.SetActive(true);
+    }
+
+}
